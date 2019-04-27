3ff45bef85b6ca6e610c32f3dc197fcf7782a254
Regansmash33 <Regansmash33@gmail.com>
2019-04-27 16:57:26 -0400

    0.1.2 Update
    Updated CSS

--- src/js/testCal.js
+++ src/js/testCal.js
@@ -1,6 +1,6 @@
 // Call this from the developer console and you can control both instances
 var calendars = {};
-
+var theCalendarInstance = $('#myCalendar').clndr();
 
 
 
@@ -11,30 +11,48 @@ $("#validateButton").click(function() {
     var inStart = moment(start);
     var inEnd = moment(endDate);
 
-    //set up date values
-    var calFill=  inFill.format('YYYY-MM-DD');
-    var calStart= inStart.format('YYYY-MM-DD'); 
-    var calEnd= inEnd.format('YYYY-MM-DD');
+    //get month,date, and year of each event
+    var fillMonth = inFill.format("MM");
+    var fillDay = inFill.format("DD");
+    var fillYear = inFill.format("YYYY");
+    var startMonth = inStart.format("MM");
+    var startDay = inStart.format("DD");
+    var startYear = inStart.format("YYYY");
+    var endMonth = inEnd.format("MM");
+    var endDay = inEnd.format("DD");
+    var endYear = inEnd.format("YYYY");
+
+    //create string variable for CLNDR to use
+    var calFill = " ";
+    var calStart = " ";
+    var calEnd = " ";
+
+    //format event string for CLNDR use
+     fullFill = (fillYear+"-"+fillMonth+"-"+fillDay);
+     fullStart = (startYear+"-"+startMonth+"-"+startDay);
+     fullEnd = (endYear+"-"+endMonth+"-"+endDay);
+
+     //create string for event
+     var calFill = String(fullFill);
+     var calStart= String(fulStart);
+     var calEnd = String(fullEnd);
+
 
     // Set up the events array
     var eventsArray = [
         {
             title: 'Prescription Filled',
-            date: String(calFill)
+            date: calFill
         }, {
             title: 'Prescription Started',
-            date: String(calStart)
+            date: String(calStart).
         }, {
             title: 'Prescription End',
             date: String(calEnd)
         }
     ];
 
-    calendars.clndr1 = $('.cal1').clndr({
-        events: eventsArray,
-        showAdjacentMonths: true,
-        adjacentDaysChangeMonth: false
-    });
+
 
 });
     
